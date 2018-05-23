8dba8e2bfaba47cd47c1f65226052b7ac59b347d
rrsegnini <roberto.t.x@hotmail.com>
2018-05-22 23:47:08 -0600

    Search Screen in process

--- app/src/main/java/cr/ac/tec/ec/cines35mm/SearchActivity.java
+++ app/src/main/java/cr/ac/tec/ec/cines35mm/SearchActivity.java
@@ -1,7 +1,10 @@
 package cr.ac.tec.ec.cines35mm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public void searchButtonOnClick(View v){
+        Button fav = findViewById(R.id.main_btnFav);
+        Intent search_screen = new Intent(this, SearchActivity.class);
+        startActivity(search_screen);
     }
 }
